3f3e98159a22b2fe705dbee6619e68f5a7055cad
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2015-08-20 00:48:27 +0200

    - Added Logging constants

---
+++ Core/LoggingConstants.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LoggingConstants.cs" company="Robert Logiewa">
+//   The MIT License (MIT)
+//   
+//   Copyright (c) 2015 Robert Logiewa
+// </copyright>
+// <summary>
+//   Contains logging wide constants.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SkyrimCompileHelper.Core
+{
+    /// <summary>Contains logging wide constants.</summary>
+    public static class LoggingConstants
+    {
+        /// <summary>The general category.</summary>
+        public const string CategoryGeneralConst = "General";
+
+        /// <summary>The compiler category.</summary>
+        public const string CategoryCompilerConst = "Compiler";
+
+        /// <summary>The error category.</summary>
+        public const string CategoryErrorConst = "Error";
+    }
+}
