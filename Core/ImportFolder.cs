613c981c3c394b859694d903ec55a193a4c93ec9
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2015-08-21 14:50:42 +0200

    - Changed ImportFolder class so the user can set a custom name

--- Core/ImportFolder.cs
+++ Core/ImportFolder.cs
@@ -13,29 +13,31 @@ namespace SkyrimCompileHelper.Core
 {
     using System.IO;
 
-    using Newtonsoft.Json;
-
     using PropertyChanged;
 
     /// <summary>Represents an import folder.</summary>
     [ImplementPropertyChanged]
     public class ImportFolder
     {
-        /// <summary>Gets the folder name.</summary>
-        [JsonIgnore]
-        public string Name
+        /// <summary>Initialises a new instance of the <see cref="ImportFolder"/> class. </summary>
+        /// <param name="path">The path to the import folder.</param>
+        public ImportFolder(string path)
         {
-            get
-            {
-                if (this.FolderPath == null)
-                {
-                    return string.Empty;
-                }
-
-                string path = Path.GetFullPath(this.FolderPath).TrimEnd(Path.DirectorySeparatorChar);
-                return Path.GetFileName(path);
-            }
+            this.FolderPath = path;
+            this.Name = Path.GetFileName(Path.GetFullPath(this.FolderPath).TrimEnd(Path.DirectorySeparatorChar));
         }
+
+        /// <summary>Initialises a new instance of the <see cref="ImportFolder"/> class. </summary>
+        /// <param name="path">The path to the import folder.</param>
+        /// <param name="name">The display name of the import folder.</param>
+        public ImportFolder(string path, string name)
+        {
+            this.FolderPath = path;
+            this.Name = name;
+        }
+
+        /// <summary>Gets or sets the folder name.</summary>
+        public string Name { get; set; }
 
         /// <summary>Gets or sets the path of the current <see cref="ImportFolder"/>.</summary>
         public string FolderPath { get; set; }
